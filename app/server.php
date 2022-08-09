d974231e54c460b98e2442671e3170e6f4c24458
rendy <rendy>
2022-08-09 12:46:47 +0700

    use Basic Swoole for Websocket service

--- app/server.php
+++ app/server.php
@@ -1,25 +1,40 @@
-#!/usr/bin/env php
 <?php
 
-declare(strict_types=1);
-
+use Swoole\WebSocket\Server;
 use Swoole\Http\Request;
-use Swoole\Http\Response;
-use Swoole\Http\Server;
+use Swoole\WebSocket\Frame;
 
-$http = new Server("0.0.0.0", 9502);
+$server = new Server("0.0.0.0", 9502);
 
-$http->on(
-    "start",
-    function (Server $http) {
-        echo "Swoole HTTP server is started.\n";
-    }
-);
-$http->on(
-    "request",
-    function (Request $request, Response $response) {
-        $response->end("Hello, Worlds!\n");
-    }
-);
+$server->on("Start", function(Server $server)
+{
+    echo "Swoole WebSocket Server is started at http://127.0.0.1:9502\n";
+});
 
-$http->start();
+$server->on('Open', function(Server $server, Swoole\Http\Request $request)
+{
+    echo "connection open: {$request->fd}\n";
+
+    $server->tick(1000, function() use ($server, $request)
+    {
+        $server->push($request->fd, json_encode(["hello", time()]));
+    });
+});
+
+$server->on('Message', function(Server $server, Frame $frame)
+{
+    echo "received message: {$frame->data}\n";
+    $server->push($frame->fd, json_encode(["hello", time()]));
+});
+
+$server->on('Close', function(Server $server, int $fd)
+{
+    echo "connection close: {$fd}\n";
+});
+
+$server->on('Disconnect', function(Server $server, int $fd)
+{
+    echo "connection disconnect: {$fd}\n";
+});
+
+$server->start();
